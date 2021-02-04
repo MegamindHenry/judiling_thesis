7b5406e7ee8d428a114ac665c1fe76377f531041
Xuefeng Luo <hakunamatatahenry@gmail.com>
2021-02-04 06:11:06 +0100

    update

--- graph/draw_wh.jl
+++ graph/draw_wh.jl
@@ -42,7 +42,7 @@ L = [
  -0.69  0.98  2.94 -0.06  1.10  2.10 # past
  -0.01  0.69 -0.26 -0.56 -0.89 -1.09 # singular
  -1.37 -0.98  0.81  0.01  0.56  0.31 # particle
- 2.15  -0.77  0.91  0.59  0.81  1.53 # talk
+  2.15 -0.77  0.91  0.59  0.81  1.53 # talk
 ]
 
 S1 = L[1:1,:] + L[2:2,:]            # walk
@@ -116,8 +116,8 @@ function wh_learn(
   W, T
 end
 
-F, FT = wh_learn(C, S, eta=0.0001, n_epochs=1000)
-G, GT = wh_learn(S, C, eta=0.0001, n_epochs=1000)
+F, FT = wh_learn(C, S, eta=0.001, n_epochs=1000)
+G, GT = wh_learn(S, C, eta=0.001, n_epochs=1000)
 
 Chat = S * G
 Shat = C * F
@@ -127,3 +127,37 @@ b = cor(Shat, S, dims=2)
 
 # a = round.(a, digits=3)
 # b = round.(b, digits=3)
+using Plots
+
+# y = [FT[1,1,:], FT[2,1,:], FT[3,1,:]. FT[4,1,:], FT[5,1,:], FT[6,1,:], 
+#     FT[7,1,:], FT[8,1,:], FT[9,1,:], FT[10,1,:], FT[11,1,:]]
+
+F1 = deepcopy(FT[1,1,:])
+F2 = deepcopy(FT[3,1,:])
+F3 = deepcopy(FT[4,1,:])
+F4 = deepcopy(FT[5,1,:])
+F5 = deepcopy(FT[7,1,:])
+F6 = deepcopy(FT[10,1,:])
+y = [F1, F2, F3, F4, F5, F6]
+
+# y = [FT[i,1,1:1000] for i in 1:2]
+# data = [[0.0001,0.0001], [0.0002,0.0002]]
+
+x = 1:1000
+
+# x = 1:10
+# y = rand(10, 2)
+
+plot(
+  x,
+  y,
+  # yscale=:log10,
+  # label=["R" "Julia" "R" "Julia" "R" "Julia" "R" "Julia" "R" "Julia" "R"],
+  label=[" #wa & wal" " alk" " lk#" " lks & ks#" " lke, ked & ed#" " #ta & tal"],
+  # lw=3,
+  grid = (:y, :olivedrab, :dot, 1, 0.9),
+  legend=:bottomleft)
+# 
+ylabel!("activation weights for S1")
+xlabel!("number of training epochs")
+savefig(joinpath(@__DIR__, "wh.pdf"))
