bb09d15522b2a6e21e4415605b174e7c14249329
Xuefeng Luo <hakunamatatahenry@gmail.com>
2021-03-15 14:21:21 +0100

    update

--- presentation/small_demo.R
+++ presentation/small_demo.R
@@ -1,23 +1,5 @@
-A = matrix(rexp(2*2*10000*1000, rate=.1), ncol=2*1000)
-B = matrix(rexp(2*2*1000*1000, rate=.1), ncol=2*1000)
-C = matrix(rexp(2*2*1000*1000, rate=.1), ncol=2*1000)
-
 start.time <- Sys.time()
-AA = A %*% B
+F = ginv(S) %*% C1
 end.time <- Sys.time()
 time.taken <- end.time - start.time
 print(time.taken)
-
-library(MASS)
-start.time <- Sys.time()
-AA = ginv(A)
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-print(time.taken)
-
-start.time <- Sys.time()
-AA = cor(B)
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-print(time.taken)
-
